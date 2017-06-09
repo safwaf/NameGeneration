0b2c0d2deb4bf6320699c84361b8ba7322efab0c
safwaf <safwafsafwaf@gmail.com>
2017-06-09 12:44:46 -0600

    Name Generator first pass
    generates decent names. in future, alter it to detect obnoxious consonant clusters and eliminate them

---
+++ NameGeneration/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NameGeneration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 116);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 142);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 168);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 194);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ NameGeneration/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameGeneration
+{
+
+    public partial class Form1 : Form
+    {
+        Generator g = new Generator();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = g.NewName();
+            textBox2.Text = g.NewName();
+            textBox3.Text = g.NewName();
+            textBox4.Text = g.NewName();
+            textBox5.Text = g.NewName();
+            textBox6.Text = g.NewName();
+            textBox7.Text = g.NewName();
+            textBox8.Text = g.NewName();
+        }
+    }
+    public class Generator
+    {
+        Random r = new Random();
+        string[] Medial = new string[11] { "b", "d", "f", "g", "j", "r", "s", "sh", "th", "x", "z" };   //
+        string[] sMedial = new string[11] { "c", "ch", "h", "k", "l", "m", "n", "p", "t", "w", "v" };
+        string[] vowel = new string[6] { "a", "e", "i", "o", "u", "y"};
+
+        public string NewName()
+        {
+            string name = "";
+            int sLength = r.Next(2) + 1; //names are one, two or 3 syllables
+             for (int i = 0; i<sLength; i++)
+            {
+                name = name + NewSyllable();
+            }
+            return name;
+        }
+
+        string NewVowel()
+        {
+            return vowel[r.Next(6)];
+        }
+
+        string NewSyllable()
+        {
+
+            //syllables always contain a vowel, and usually contain an onset and coda
+            string syllable = "";
+            if (r.Next(4) != 0) syllable = syllable + NewOnset();
+            syllable = syllable + NewVowel();
+            if (r.Next(4) != 0) syllable = syllable + NewCoda();
+            return syllable;
+        }
+
+        string NewOnset()
+        {
+            string medial = "";
+            string initial = "";
+            string final = "";
+            //an onset consists of a medial consonant, that might be proceeded by an s or followed by a liquid consonant
+            //the double-if structure leads the algorithm to favour 2-consonant clusters. This is easy to change
+            if (r.Next(5) == 1)
+            {
+                initial = "s";
+                medial = sMedial[r.Next(11)];
+            }
+            else if (r.Next(2) == 1) medial = sMedial[r.Next(11)];
+            else medial = Medial[r.Next(11)];
+
+            if (r.Next(2) == 1)
+            {
+                switch (medial)
+                {
+                    case "h": break;
+                    case "j": break;
+                    case "l": break;
+                    case "m": break;
+                    case "n": break;
+                    case "q": break;
+                    case "r": break;
+                    case "x": break;
+                    case "z": break;
+                    default:
+                        if (r.Next(2) == 1) final = "l";
+                        else final = "r";
+                        break;
+                }
+            }
+                return initial + medial + final;
+        }
+
+        string NewCoda()
+        {
+            string medial = "";
+            string initial = "";
+            string liquid = "";
+
+            if (r.Next(5) == 1)
+            {
+                initial = "s";
+                medial = sMedial[r.Next(11)];
+            }
+            else if (r.Next(2) == 1) medial = sMedial[r.Next(11)];
+            else medial = Medial[r.Next(11)];
+
+            if (r.Next(2) == 1)
+            {
+            if (r.Next(2) == 1) liquid = "l";
+            else liquid = "r";
+            }
+            return liquid + initial + medial;
+        }
+
+    }
+}
